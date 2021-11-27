5adb35c55aada08e85505bd844df3d889713ed31
ljacob23 <ljacob23@live.unc.edu>
2021-11-27 18:39:58 -0500

    sadfje

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -169,7 +169,7 @@ export const filter = (arr, test) => {
  *   odd numbers. Use the "everyEven" function in this function.
  */
 export const allEvensAreOdd = (arr) => {
- return everyEven(arr, x %2 != 0);
+ return everyEven(arr, x=>x %2 != 0);
 };
 
 
@@ -179,7 +179,7 @@ export const allEvensAreOdd = (arr) => {
  *   array is an odd number. Use the "someEven" function in this function.
  */
 export const anEvenIsOdd = (arr) => {
-    return someEven(arr, x %2 != 0);
+    return someEven(arr, x=>x %2 != 0);
 };
 
 
